548071ec884198cf9cf56b9a2fb1ec0037777ab2
Joe Hayes <joerussellhayes@gmail.com>
2018-03-03 19:04:30 -0600

    thank you test

---
+++ assignments/lesson-8/thank_you.php
@@ -0,0 +1,72 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+/*
+
+Thank you for choosing FormToEmail by FormToEmail.com
+
+Version 2.6 May 2014
+
+COPYRIGHT FormToEmail.com 2003 - 2009
+
+You are not permitted to sell this script, but you can use it, copy it or distribute it, providing that you do not delete this copyright notice, and you do not remove any reference or links to FormToEmail.com
+
+For support, please visit: http://formtoemail.com/support/
+
+SETUP INSTRUCTIONS
+
+Enter your email address.
+
+Enter the email address below to send the contents of the form to.  You can enter more than one email address separated by commas, like so: $my_email = "info@example.com"; or $my_email = "bob@example.com,sales@example.co.uk,jane@example.com";
+
+*/
+
+$my_email = "joerussellhayes@gmail.com";
+
+/*
+
+Optional.  Enter a From: email address.  Only do this if you know you need to.  By default, the email you get from the script will show the visitor's email address as the From: address.  In most cases this is desirable.  On the majority of setups this won't be a problem but a minority of hosts insist that the From: address must be from a domain on the server.  For example, if you have the domain example.com hosted on your server, then the From: email address must be something@example.com (See your host for confirmation).  This means that your visitor's email address will not show as the From: address, and if you hit "Reply" to the email from the script, you will not be replying to your visitor.  You can get around this by hard-coding a From: address into the script using the configuration option below.  Enabling this option means that the visitor's email address goes into a Reply-To: header, which means you can hit "Reply" to respond to the visitor in the conventional way.  (You can also use this option if your form does not collect an email address from the visitor, such as a survey, for example, and a From: address is required by your email server.)  The default value is: $from_email = "";  Enter the desired email address between the quotes, like this example: $from_email = "contact@example.com";  In these cases, it is not uncommon for the From: ($from_email) address to be the same as the To: ($my_email) address, which on the face of it appears somewhat goofy, but that's what some hosts require.
+
+*/
+
+$from_email = "";
+
+/*
+
+Optional.  Enter the continue link to offer the user after the form is sent.  If you do not change this, your visitor will be given a continue link to your homepage.
+
+If you do change it, remove the "/" symbol below and replace with the name of the page to link to, eg: "mypage.htm" or "http://www.elsewhere.com/page.htm"
+
+*/
+
+//CHANGED BY PAUL CHENEY
+$continue=$_REQUEST['redirect'];
+if ($continue == null) 
+{
+	$continue = "thanks.html";
+}
+
+
+
+$errors = array();
+
+<!doctype html>
+
+<html>
+
+<head>
+<title>Thank you</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#ffffff" text="#000000">
+
+<div>
+<center>
+<<a href="thanks.html">FormToEmail.com</a>
+</center>
+</div>
+
+</body>
+</html>
